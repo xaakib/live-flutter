1f98f8447c6b5b1424fd9de9381aa120c5f5a82e
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-04-20 02:00:28 +0600

    chnaged

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class EnvironmentConfig {
-  static const String streamAPIKey = String.fromEnvironment("stream-api");
-  static const String muxApi = String.fromEnvironment("mux-api");
+  static const String streamAPIKey = String.fromEnvironment("xmam3zhg3vxrsr5a4jz8k755ddq5r8uvgfsjxsbfce9wucagydf8z64bynwtge5p");
+  static const String muxApi = String.fromEnvironment("6b02f15b-508e-4c17-8bd4-69c76880d6d5");
 }
